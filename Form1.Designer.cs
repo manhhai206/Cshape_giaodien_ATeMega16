094b5cd4fa1ae16bed8bcaa1e5bb467b5b2cf8ee
Manh Hai <manhtronghai2062002@gmail.com>
2023-09-06 20:59:26 +0700

    manhhai

---
+++ Form1.Designer.cs
@@ -0,0 +1,392 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lisPort = new System.Windows.Forms.ComboBox();
+            this.lisBaul = new System.Windows.Forms.ComboBox();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.txtMinValue = new System.Windows.Forms.TextBox();
+            this.txtAveValue = new System.Windows.Forms.TextBox();
+            this.txtMaxValue = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.txtnhietdo = new System.Windows.Forms.TextBox();
+            this.btnLED1 = new System.Windows.Forms.Button();
+            this.btnLED2 = new System.Windows.Forms.Button();
+            this.btnLED3 = new System.Windows.Forms.Button();
+            this.btnLED4 = new System.Windows.Forms.Button();
+            this.btnLED5 = new System.Windows.Forms.Button();
+            this.btnLED6 = new System.Windows.Forms.Button();
+            this.btnLED7 = new System.Windows.Forms.Button();
+            this.btnLED8 = new System.Windows.Forms.Button();
+            this.exit = new System.Windows.Forms.Button();
+            this.zedGraphControl1 = new ZedGraph.ZedGraphControl();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "COM";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(8, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "BAUDRATE";
+            // 
+            // lisPort
+            // 
+            this.lisPort.FormattingEnabled = true;
+            this.lisPort.Location = new System.Drawing.Point(83, 11);
+            this.lisPort.Name = "lisPort";
+            this.lisPort.Size = new System.Drawing.Size(99, 21);
+            this.lisPort.TabIndex = 2;
+            // 
+            // lisBaul
+            // 
+            this.lisBaul.FormattingEnabled = true;
+            this.lisBaul.Location = new System.Drawing.Point(83, 40);
+            this.lisBaul.Name = "lisBaul";
+            this.lisBaul.Size = new System.Drawing.Size(99, 21);
+            this.lisBaul.TabIndex = 3;
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(205, 12);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(75, 49);
+            this.btnConnect.TabIndex = 4;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived);
+            // 
+            // txtMinValue
+            // 
+            this.txtMinValue.Location = new System.Drawing.Point(123, 328);
+            this.txtMinValue.Name = "txtMinValue";
+            this.txtMinValue.Size = new System.Drawing.Size(100, 20);
+            this.txtMinValue.TabIndex = 8;
+            this.txtMinValue.TextChanged += new System.EventHandler(this.txtMinValue_TextChanged);
+            // 
+            // txtAveValue
+            // 
+            this.txtAveValue.Location = new System.Drawing.Point(123, 363);
+            this.txtAveValue.Name = "txtAveValue";
+            this.txtAveValue.Size = new System.Drawing.Size(100, 20);
+            this.txtAveValue.TabIndex = 9;
+            // 
+            // txtMaxValue
+            // 
+            this.txtMaxValue.Location = new System.Drawing.Point(123, 395);
+            this.txtMaxValue.Name = "txtMaxValue";
+            this.txtMaxValue.Size = new System.Drawing.Size(100, 20);
+            this.txtMaxValue.TabIndex = 10;
+            this.txtMaxValue.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(10, 335);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "MIN VALUE";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(7, 366);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(96, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "AVERAGE VALUE";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(10, 398);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(68, 13);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "MAX VALUE";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.ImageKey = "(none)";
+            this.btnStart.Location = new System.Drawing.Point(78, 98);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(91, 29);
+            this.btnStart.TabIndex = 17;
+            this.btnStart.Text = "MODE";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // txtnhietdo
+            // 
+            this.txtnhietdo.Location = new System.Drawing.Point(82, 184);
+            this.txtnhietdo.Name = "txtnhietdo";
+            this.txtnhietdo.Size = new System.Drawing.Size(100, 20);
+            this.txtnhietdo.TabIndex = 18;
+            // 
+            // btnLED1
+            // 
+            this.btnLED1.Location = new System.Drawing.Point(82, 221);
+            this.btnLED1.Name = "btnLED1";
+            this.btnLED1.Size = new System.Drawing.Size(40, 40);
+            this.btnLED1.TabIndex = 19;
+            this.btnLED1.Text = "LED1 ";
+            this.btnLED1.UseVisualStyleBackColor = true;
+            this.btnLED1.Click += new System.EventHandler(this.btnLED1_Click);
+            // 
+            // btnLED2
+            // 
+            this.btnLED2.Location = new System.Drawing.Point(83, 267);
+            this.btnLED2.Name = "btnLED2";
+            this.btnLED2.Size = new System.Drawing.Size(40, 40);
+            this.btnLED2.TabIndex = 20;
+            this.btnLED2.Text = "LED2";
+            this.btnLED2.UseVisualStyleBackColor = true;
+            this.btnLED2.Click += new System.EventHandler(this.btnLED2_Click);
+            // 
+            // btnLED3
+            // 
+            this.btnLED3.Location = new System.Drawing.Point(129, 221);
+            this.btnLED3.Name = "btnLED3";
+            this.btnLED3.Size = new System.Drawing.Size(40, 40);
+            this.btnLED3.TabIndex = 21;
+            this.btnLED3.Text = "LED3";
+            this.btnLED3.UseVisualStyleBackColor = true;
+            this.btnLED3.Click += new System.EventHandler(this.btnLED3_Click);
+            // 
+            // btnLED4
+            // 
+            this.btnLED4.Location = new System.Drawing.Point(129, 267);
+            this.btnLED4.Name = "btnLED4";
+            this.btnLED4.Size = new System.Drawing.Size(40, 40);
+            this.btnLED4.TabIndex = 22;
+            this.btnLED4.Text = "LED4";
+            this.btnLED4.UseVisualStyleBackColor = true;
+            this.btnLED4.Click += new System.EventHandler(this.btnLED4_Click);
+            // 
+            // btnLED5
+            // 
+            this.btnLED5.Location = new System.Drawing.Point(175, 221);
+            this.btnLED5.Name = "btnLED5";
+            this.btnLED5.Size = new System.Drawing.Size(40, 40);
+            this.btnLED5.TabIndex = 23;
+            this.btnLED5.Text = "LED5";
+            this.btnLED5.UseVisualStyleBackColor = true;
+            this.btnLED5.Click += new System.EventHandler(this.btnLED5_Click);
+            // 
+            // btnLED6
+            // 
+            this.btnLED6.Location = new System.Drawing.Point(175, 267);
+            this.btnLED6.Name = "btnLED6";
+            this.btnLED6.Size = new System.Drawing.Size(40, 40);
+            this.btnLED6.TabIndex = 24;
+            this.btnLED6.Text = "LED6";
+            this.btnLED6.UseVisualStyleBackColor = true;
+            this.btnLED6.Click += new System.EventHandler(this.btnLED6_Click);
+            // 
+            // btnLED7
+            // 
+            this.btnLED7.Location = new System.Drawing.Point(221, 221);
+            this.btnLED7.Name = "btnLED7";
+            this.btnLED7.Size = new System.Drawing.Size(40, 40);
+            this.btnLED7.TabIndex = 25;
+            this.btnLED7.Text = "LED7";
+            this.btnLED7.UseVisualStyleBackColor = true;
+            this.btnLED7.Click += new System.EventHandler(this.btnLED7_Click);
+            // 
+            // btnLED8
+            // 
+            this.btnLED8.Location = new System.Drawing.Point(221, 267);
+            this.btnLED8.Name = "btnLED8";
+            this.btnLED8.Size = new System.Drawing.Size(40, 40);
+            this.btnLED8.TabIndex = 26;
+            this.btnLED8.Text = "LED8";
+            this.btnLED8.UseVisualStyleBackColor = true;
+            this.btnLED8.Click += new System.EventHandler(this.btnLED8_Click);
+            // 
+            // exit
+            // 
+            this.exit.Location = new System.Drawing.Point(31, 436);
+            this.exit.Name = "exit";
+            this.exit.Size = new System.Drawing.Size(135, 23);
+            this.exit.TabIndex = 28;
+            this.exit.Text = "EXIT PROGRAM";
+            this.exit.UseVisualStyleBackColor = true;
+            this.exit.Click += new System.EventHandler(this.exit_Click);
+            // 
+            // zedGraphControl1
+            // 
+            this.zedGraphControl1.Location = new System.Drawing.Point(310, 12);
+            this.zedGraphControl1.Name = "zedGraphControl1";
+            this.zedGraphControl1.ScrollGrace = 0D;
+            this.zedGraphControl1.ScrollMaxX = 0D;
+            this.zedGraphControl1.ScrollMaxY = 0D;
+            this.zedGraphControl1.ScrollMaxY2 = 0D;
+            this.zedGraphControl1.ScrollMinX = 0D;
+            this.zedGraphControl1.ScrollMinY = 0D;
+            this.zedGraphControl1.ScrollMinY2 = 0D;
+            this.zedGraphControl1.Size = new System.Drawing.Size(611, 447);
+            this.zedGraphControl1.TabIndex = 29;
+            this.zedGraphControl1.UseExtendedPrintDialog = true;
+            this.zedGraphControl1.Load += new System.EventHandler(this.zedGraphControl1_Load);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::WindowsFormsApp1.Properties.Resources.on;
+            this.pictureBox3.Location = new System.Drawing.Point(11, 95);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(32, 32);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 30;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::WindowsFormsApp1.Properties.Resources.png_clipart_ampoule_lumiere_creativite_ampoule_a_incandescence_idee_dessin_logo_bijoux_de_corps;
+            this.pictureBox2.Location = new System.Drawing.Point(11, 221);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(52, 86);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 7;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::WindowsFormsApp1.Properties.Resources.tải_xuống;
+            this.pictureBox1.Location = new System.Drawing.Point(11, 149);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(49, 55);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Desktop;
+            this.ClientSize = new System.Drawing.Size(933, 471);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.zedGraphControl1);
+            this.Controls.Add(this.exit);
+            this.Controls.Add(this.btnLED8);
+            this.Controls.Add(this.btnLED7);
+            this.Controls.Add(this.btnLED6);
+            this.Controls.Add(this.btnLED5);
+            this.Controls.Add(this.btnLED4);
+            this.Controls.Add(this.btnLED3);
+            this.Controls.Add(this.btnLED2);
+            this.Controls.Add(this.btnLED1);
+            this.Controls.Add(this.txtnhietdo);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtMaxValue);
+            this.Controls.Add(this.txtAveValue);
+            this.Controls.Add(this.txtMinValue);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.lisBaul);
+            this.Controls.Add(this.lisPort);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox lisPort;
+        private System.Windows.Forms.ComboBox lisBaul;
+        private System.Windows.Forms.Button btnConnect;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.TextBox txtMinValue;
+        private System.Windows.Forms.TextBox txtAveValue;
+        private System.Windows.Forms.TextBox txtMaxValue;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.TextBox txtnhietdo;
+        private System.Windows.Forms.Button btnLED1;
+        private System.Windows.Forms.Button btnLED2;
+        private System.Windows.Forms.Button btnLED3;
+        private System.Windows.Forms.Button btnLED4;
+        private System.Windows.Forms.Button btnLED5;
+        private System.Windows.Forms.Button btnLED6;
+        private System.Windows.Forms.Button btnLED7;
+        private System.Windows.Forms.Button btnLED8;
+        private System.Windows.Forms.Button exit;
+        private ZedGraph.ZedGraphControl zedGraphControl1;
+        private System.Windows.Forms.PictureBox pictureBox3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,318 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO.Ports;
+using ZedGraph;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        int minvalue = 100;
+        int maxvalue = 0;
+        double average = 0;
+        int countpoint = 0;
+        public Form1()
+        {
+            InitializeComponent();
+             minvalue = 100;
+             maxvalue = 0;
+             average = 0;
+             countpoint = 0;
+        }
+        string[] pause = { "1200", "4800", "2400", "9600", "38400", "57600", "14880" };
+
+        private void lisPort_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+            string[] listnamecom = SerialPort.GetPortNames();
+            lisPort.Items.AddRange(listnamecom);
+            lisBaul.Items.AddRange(pause);
+
+            GraphPane grap = zedGraphControl1.GraphPane;
+            grap.Title.Text = " Biểu đồ nhiệt độ ";
+            grap.XAxis.Title.Text = "TIME";
+            grap.YAxis.Title.Text = "DEGREE";
+
+            RollingPointPairList list1 = new RollingPointPairList(60000);
+
+            LineItem duongline1 = grap.AddCurve("DEGREE", list1, System.Drawing.Color.Blue, SymbolType.Circle);
+
+            grap.XAxis.Scale.Min = 0;
+            grap.XAxis.Scale.Max = 100;
+            grap.XAxis.Scale.MinorStep = 1;
+            grap.XAxis.Scale.MajorStep = 5;
+
+            grap.YAxis.Scale.Min = 0;
+            grap.YAxis.Scale.Max = 100;
+            grap.YAxis.Scale.MinorStep = 1;
+            grap.YAxis.Scale.MajorStep = 5;
+
+            zedGraphControl1.AxisChange();
+        }
+        int tong = 0;
+        public void draw(double line1)
+        {
+            if (zedGraphControl1.GraphPane.CurveList.Count <= 0)
+                return;
+            LineItem duongline1 = zedGraphControl1.GraphPane.CurveList[0] as LineItem;
+            if (duongline1 == null)
+                return;
+            IPointListEdit list1 = duongline1.Points as IPointListEdit;
+            if (list1 == null)
+                return;
+            list1.Add(tong, line1);
+            zedGraphControl1.AxisChange();
+            zedGraphControl1.Invalidate();
+            tong += 2;
+
+
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            if (serialPort1.IsOpen == true)
+            {
+                serialPort1.Close();
+                btnConnect.Text = "Connect";
+            }
+            else if (serialPort1.IsOpen == false)
+            {
+                serialPort1.PortName = lisPort.Text;
+                serialPort1.BaudRate = int.Parse(lisBaul.Text);
+                serialPort1.Open();
+                btnConnect.Text = "Disconnect";
+
+            }
+        }
+
+        private void btnNd_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void serialPort1_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            string data = "";
+            data = serialPort1.ReadLine ();
+                 
+            if (data.Length > 0)
+            {
+                Invoke(new MethodInvoker(() => txtnhietdo.Text = data + "°C"));
+                try
+                {
+                    double datapoint = double.Parse(data);
+                    Invoke(new MethodInvoker(() => draw(Convert.ToDouble(data))));
+                    Invoke(new MethodInvoker(() =>
+                    {
+                        if (datapoint > maxvalue)
+                        {
+                            maxvalue = (int)datapoint;
+                            txtMaxValue.Text = maxvalue.ToString();
+                        }
+                        if (datapoint < minvalue)
+                        {
+                           minvalue = (int)datapoint;
+                           txtMinValue.Text = minvalue.ToString();
+                        }
+                        average = (average * countpoint + datapoint) / (countpoint + 1);
+                        countpoint++;
+                        double roundedNumber = Math.Round(average, 3);
+                        txtAveValue.Text = roundedNumber.ToString();
+                    }));
+
+                }
+                catch
+                {
+
+                }
+                
+            }         
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+        bool tb1 = true;
+        private void btnLED1_Click(object sender, EventArgs e)
+        {
+            if (tb1 == true)
+            {
+                serialPort1.Write("a1?");
+                btnLED1.Text = "ON";
+                tb1 = false;
+            }
+            else
+            {
+                serialPort1.Write("a0?");
+                btnLED1.Text = "OFF";
+                tb1 = true;
+            }
+        }
+        bool tb2 = true;
+        private void btnLED2_Click(object sender, EventArgs e)
+        {
+            if (tb2 == true)
+            {
+                serialPort1.Write("b1?");
+                btnLED2.Text = "ON";
+                tb2 = false;
+            }
+            else
+            {
+                serialPort1.Write("b0?");
+                btnLED2.Text = "OFF";
+                tb2 = true;
+            }
+        }
+        bool tb3 = true;
+        private void btnLED3_Click(object sender, EventArgs e)
+        {
+            if (tb3 == true)
+            {
+                serialPort1.Write("c1?");
+                btnLED3.Text = "ON";
+                tb3 = false;
+            }
+            else
+            {
+                serialPort1.Write("c0?");
+                btnLED3.Text = "OFF";
+                tb3 = true;
+            }
+        }
+        bool tb4 = true;
+        private void btnLED4_Click(object sender, EventArgs e)
+        {
+            if (tb4 == true)
+            {
+                serialPort1.Write("d1?");
+                btnLED4.Text = "ON";
+                tb4 = false;
+            }
+            else
+            {
+                serialPort1.Write("d0?");
+                btnLED4.Text = "OFF";
+                tb4 = true;
+            }
+        }
+        bool tb5 = true;
+        private void btnLED5_Click(object sender, EventArgs e)
+        {
+            if (tb5 == true)
+            {
+                serialPort1.Write("e1?");
+                btnLED5.Text = "ON";
+                tb5 = false;
+            }
+            else
+            {
+                serialPort1.Write("e0?");
+                btnLED5.Text = "OFF";
+                tb5 = true;
+            }
+        }
+        bool tb6 = true;
+        private void btnLED6_Click(object sender, EventArgs e)
+        {
+            if (tb6 == true)
+            {
+                serialPort1.Write("f1?");
+                btnLED6.Text = "ON";
+                tb6 = false;
+            }
+            else
+            {
+                serialPort1.Write("f0?");
+                btnLED6.Text = "OFF";
+                tb6 = true;
+            }
+        }
+        bool tb7 = true;
+        private void btnLED7_Click(object sender, EventArgs e)
+        {
+            if (tb7 == true)
+            {
+                serialPort1.Write("g1?");
+                btnLED7.Text = "ON";
+                tb7 = false;
+            }
+            else
+            {
+                serialPort1.Write("g0?");
+                btnLED7.Text = "OFF";
+                tb7 = true;
+            }
+        }
+        bool tb8 = true;
+        private void btnLED8_Click(object sender, EventArgs e)
+        {
+            if (tb8 == true)
+            {
+                serialPort1.Write("h1?");
+                btnLED8.Text = "ON";
+                tb8 = false;
+            }
+            else
+            {
+                serialPort1.Write("h0?");
+                btnLED8.Text = "OFF";
+                tb8 = true;
+            }
+        }
+
+        private void txtMinValue_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void exit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void zedGraphControl1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        bool start = true;
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if (start == true)
+            {
+                serialPort1.Write("sp?");
+                btnStart.Text = "START";
+                pictureBox3.Image= global::WindowsFormsApp1.Properties.Resources.off;
+                start = false;
+            }
+            else
+            {
+                serialPort1.Write("st?");
+                btnStart.Text = "STOP";
+                pictureBox3.Image = global::WindowsFormsApp1.Properties.Resources.on;
+                start = true;
+
+
+            }
+        }
+
+
+
+
+    }
+}
+
+
+
